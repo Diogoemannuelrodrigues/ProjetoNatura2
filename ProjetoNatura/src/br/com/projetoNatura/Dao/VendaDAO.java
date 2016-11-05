cccfea581a999c6486eb4617b41b7121a4ae3c94
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2016-11-05 10:23:12 -0200

    Criacao da venda

---
+++ ProjetoNatura/src/br/com/projetoNatura/Dao/VendaDAO.java
@@ -0,0 +1,59 @@
+package br.com.projetoNatura.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.projetoNatura.conexao.Conexao;
+import br.com.projetoNatura.entidade.Cliente;
+import br.com.projetoNatura.entidade.Venda;
+
+public class VendaDAO {
+
+	public void realizaVenda(Venda venda) {
+
+		try {
+			Connection con = Conexao.getConnection();
+			PreparedStatement stm = con.prepareStatement("insert into venda (codigoProduto, idCliente, descricao) values (?,?,?)");
+			stm.setInt(1, venda.getProduto().getCodigoProduto());
+			stm.setInt(2, venda.getCliente().getId());
+			stm.setString(3, venda.getDescricao());
+			stm.execute();
+			stm.close();
+			con.close();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+}
+		
+		public List<Venda> mostrarVendas(){
+			List<Venda> vendas = new ArrayList<Venda>();
+			Venda venda = new Venda();
+			Cliente cliente = new Cliente();
+			ClienteDao dao = new ClienteDao();
+			ProdutoDao daoP = new ProdutoDao();
+			try{
+				Connection con = Conexao.getConnection();
+				PreparedStatement stm = con.prepareStatement("select * from venda "); //arrumar depois igual o do josue
+				ResultSet sul = stm.executeQuery();
+				while(sul.next()){
+					venda.setCliente(dao.consultarPorId(sul.getInt("id")));
+					venda.setProduto(daoP.buscarProdutoPorId(sul.getInt("codigoProduto")));
+					venda.setDescricao(sul.getString("descricao"));
+					vendas.add(venda);
+				}
+				sul.close();
+				stm.close();
+				con.close();
+				
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+			return vendas;
+		}
+	}
+
+
---
+++ ProjetoNatura/src/br/com/projetoNatura/entidade/Venda.java
@@ -0,0 +1,42 @@
+package br.com.projetoNatura.entidade;
+
+public class Venda {
+	
+	private Produto produto = new Produto();
+	private Cliente cliente = new Cliente();;
+	private String descricao;
+	private int idVenda;
+
+	public int getIdVenda() {
+		return idVenda;
+	}
+
+	public void setIdVenda(int idVenda) {
+		this.idVenda = idVenda;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
